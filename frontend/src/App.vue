b2030d0a2dd5acf764223a5879473583072c2d21
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-05-26 20:57:46 -0600

    componente raiz App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import Swal from 'sweetalert2'
+
+onMounted(() => {
+  // Hacer SweetAlert disponible globalmente
+  window.Swal = Swal
+})
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #0f0508;
+  color: #f0e8e0;
+  font-family: 'Segoe UI', system-ui, sans-serif;
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: #0f0508;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #c9a84c;
+  border-radius: 3px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #f0c96b;
+}
+</style>
